ed9f64e76472d2409aa226be1f64c3ebcf600940
sammy <sammylaureys@hotmail.com>
2020-10-17 18:10:14 +0200

    add initial files

---
+++ src/main/java/com/example/demo/TestingSmallSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSmallSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSmallSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/homeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class homeController {
+
+    @Value("${spring.message}")
+    private String message;
+
+    @GetMapping
+    public String text(){
+        return message;
+    }
+}
